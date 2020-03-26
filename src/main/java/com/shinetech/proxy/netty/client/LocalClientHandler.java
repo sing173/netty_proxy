6880737b10808ad1f58b82d32e5994b6a2574905
sing173 <sing173@126.com>
2020-03-26 21:42:19 +0800

    zk

--- src/main/java/com/shinetech/proxy/netty/client/LocalClientHandler.java
+++ src/main/java/com/shinetech/proxy/netty/client/LocalClientHandler.java
@@ -1,33 +1,23 @@
 package com.shinetech.proxy.netty.client;
 
 import com.alibaba.fastjson.JSON;
-import com.shinetech.common.utils.PropertyUtils;
-import com.shinetech.common.utils.ZKClient;
 import com.shinetech.proxy.netty.common.Constant;
 import com.shinetech.proxy.netty.common.HttpUtils;
 import com.shinetech.proxy.netty.common.buffer.RteClientResponseCache;
 import com.shinetech.proxy.netty.message.Message;
 import com.shinetech.rte.netty.client.RteClient;
-import com.shinetech.rte.netty.client.RteClientStart;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpConstants;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -35,132 +25,35 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 客户端响应请求的内部处理程序
  */
 
-public class LocalClientHandler extends ChannelInboundHandlerAdapter implements Watcher {
+public class LocalClientHandler extends ChannelInboundHandlerAdapter {
 
 
     private static final Logger logger = LoggerFactory.getLogger(LocalClientHandler.class);
 
-    /**
-     * 链接决策引擎的客户端集合
-     */
-    private ConcurrentMap<String, RteClient> rteClientPool = new ConcurrentHashMap<>();
-    private List<String> keys = new ArrayList<>();
+
     private AtomicInteger roundRobin = new AtomicInteger(0);
     private static final int MAX_VALUE = 100000;
     private static final int MIN_VALUE = 0;
 
     private LocalClient client;
     private RteClientResponseCache responseCache;
-    private String zkAddress;
-    private ZKClient zkClient;
+    private LocalClientProcessor processor;
+
 
     public LocalClientHandler(LocalClient client) {
         this.client = client;
-//        Properties properties = PropertyUtils.getInstance();
-//        this.zkAddress = (String) properties.get(Constant.ZOOKEEPER_ADDRESS);
-        this.zkAddress = Constant.ZOOKEEPER_ADDRESS;
         this.responseCache = RteClientResponseCache.newBuild();
+        this.processor = new LocalClientProcessor(this.responseCache);
 
-        try {
-            this.zkClient = new ZKClient(zkAddress, this);
-
-            //不存在则新建, 这个目录必须存在
-            if (!zkClient.exist(Constant.ZK_PROXY_CONFIG_PATH)) {
-                zkClient.registerService(Constant.ZK_PROXY_CONFIG_PATH, "0");
-                logger.info("create zk address：" + Constant.ZK_PROXY_CONFIG_PATH);
-            }
-
-            initClientPool();
-        } catch (IOException e) {
-            e.printStackTrace();
-            logger.error("Zookeeper init error：" + e.getMessage());
-        }
-
-        //启动一个线程，处理zk连接断开的情况
-        Runnable sendTask = new Runnable() {
-            @Override
-            public void run() {
-                for (; ; ) {
-                    logger.debug("check zookeeper Client  connection ......");
-                    // 如果zk连接不上，重新初始化zk
-                    if (!zkClient.isAvailable()) {
-                        if (zkClient.reconnect(LocalClientHandler.this)) {
-                            initClientPool();
-                        }
-                    }
-                    //休眠
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        new Thread(sendTask).start();
-    }
-
-    public void initClientPool() {
-        // 带有事件监听，创建或者删除下一层节点，都会被通知到
-        List<String> oldList = zkClient.listServices(Constant.FLINK_NETTY_SERVER, true);
-
-        if (oldList != null && !oldList.isEmpty()) {
-            logger.info("flink source netty server register info: " + oldList);
-
-            ConcurrentMap<String, RteClient> tmpClientPool = new ConcurrentHashMap<>();
-            List<String> tmpKeys = new ArrayList<>();
-
-            for (String key : oldList) {
-                String ip = "";
-                String port = "";
-                try {
-                    ip = key.split(":")[0];
-                    port = key.split(":")[1];
-                } catch (Exception e) {
-                    continue;//不是ip:port的格式，进入下一个循环
-                }
-
-                RteClient client = null;
-                // 避免重复创建连接，只要ip和端口一样，会有15s重连机制
-                if (rteClientPool.containsKey(key)) {
-                    client = rteClientPool.get(key);
-                } else {
-                    client = RteClientStart.buildClient(ip, Integer.parseInt(port), 20, 15000, responseCache);
-                }
-
-                logger.info("add flink znode:" + ip + ":" + port);
-
-                tmpClientPool.put(key, client);
-                tmpKeys.add(key);
-            }
-
-            // 直接变量替换，避免直接操作的冲突
-            rteClientPool = tmpClientPool;
-            keys = tmpKeys;
-
-
-        } else {
-            logger.error("storm netty spout server is null in zk: " + zkAddress + " , path: " + Constant.FLINK_NETTY_SERVER);
-        }
-    }
-
-    @Override
-    public void process(WatchedEvent event) {
-        String path = event.getPath();
-        logger.info("zookeeper path has changed: " + path);
-
-        if (Constant.ZK_PROXY_CONFIG_PATH.equalsIgnoreCase(path)) {
-            initClientPool();
-        }
     }
 
     public RteClient getClient() {
-        if (keys == null || keys.size() == 0) {
+        if (processor.keys == null || processor.keys.size() == 0) {
             return null;
         }
-        int index = this.getRoundRobinValue().get() % keys.size();
+        int index = this.getRoundRobinValue().get() % processor.keys.size();
         logger.info("from the client pool get index :" + index);
-        return rteClientPool.get(keys.get(index));
+        return processor.rteClientPool.get(processor.keys.get(index));
     }
 
 
